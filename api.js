d530227ca68d574035512e7f72b0bab538ee932c
Ferdeveloperr <fernandojgom1395@gmail.com>
2024-11-01 14:10:22 -0300

    change cotizaciones

--- api.js
+++ api.js
@@ -1,11 +1,10 @@
 const obtenerDatos = document.getElementById('obtenerDatos');
-const resultado = document.getElementById('resultado');
+const resultado = document.getElementById('resultadoApi');
 
 obtenerDatos.addEventListener('click', () => {
     fetch('https://api.bluelytics.com.ar/v2/latest')
         .then(response => response.json())
         .then(data => {
-            // Procesa los datos de la API aquí
             mostrarResultado(data);
         })
         .catch(error => {
@@ -15,10 +14,42 @@ obtenerDatos.addEventListener('click', () => {
 });
 
 function mostrarResultado(data) {
+    const dolarOficial = data.oficial;
+    const dolarBlue = data.blue;
+    const euroOficial = data.oficial_euro;
+    const euroBlue = data.blue_euro;
+    const lastUpdate = data.last_update;
+
     resultado.innerHTML = `
-        <h2>Datos de la API</h2>
-        <pre>${JSON.stringify(data, null, 2)}</pre>`;
+        
+        <div>
+            <h3>Dólar Oficial</h3>
+            <p>Compra: ${dolarOficial.value_buy}</p>
+            <p>Venta: ${dolarOficial.value_sell}</p>
+            <p>Promedio: ${dolarOficial.value_avg}</p>
+        </div>
+        <div>
+            <h3>Dólar Blue</h3>
+            <p>Compra: ${dolarBlue.value_buy}</p>
+            <p>Venta: ${dolarBlue.value_sell}</p>
+            <p>Promedio: ${dolarBlue.value_avg}</p>
+        </div>
+        <div>
+            <h3>Euro Oficial</h3>
+            <p>Compra: ${euroOficial.value_buy}</p>
+            <p>Venta: ${euroOficial.value_sell}</p>
+            <p>Promedio: ${euroOficial.value_avg}</p>
+        </div>
+        <div>
+            <h3>Euro Blue</h3>
+            <p>Compra: ${euroBlue.value_buy}</p>
+            <p>Venta: ${euroBlue.value_sell}</p>
+            <p>Promedio: ${euroBlue.value_avg}</p>
+        </div>
+        <p>Última actualización: ${new Date(lastUpdate).toLocaleString()}</p>
+    `;
 }
+
 
 function mostrarError() {
     resultado.innerHTML = '<p>Ocurrió un error al obtener los datos de la API.</p>';
